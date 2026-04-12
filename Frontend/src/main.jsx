0a3460d01985c804ce82d305eea10ea391d6845e
Hammad Gul <iamhammad148@gmail.com>
2026-04-12 15:39:56 +0500

    setup react query client with default options

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,10 +5,20 @@ import routes from "./Routes";
 import { Provider } from "react-redux";
 import blogifyStore from "./Redux/Store";
 import Notification from "./Components/Notification";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={blogifyStore}>
-    <Notification />
-    <RouterProvider router={routes}></RouterProvider>
-  </Provider>,
+  <QueryClientProvider client={queryClient}>
+    <Provider store={blogifyStore}>
+      <Notification />
+      <RouterProvider router={routes}></RouterProvider>
+    </Provider>
+  </QueryClientProvider>,
 );
