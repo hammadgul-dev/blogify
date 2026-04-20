bd0c2b567dc14e4ccb4c32a479f923a9ebf469e7
Hammad Gul <iamhammad148@gmail.com>
2026-04-20 23:38:33 +0500

    add userId ref and comments in blog schema

--- Backend/models/addBlogModel.js
+++ Backend/models/addBlogModel.js
@@ -2,12 +2,20 @@ import mongoose from "mongoose"
 
 let addBlogSchema = new mongoose.Schema(
   {
+    userId : { type : mongoose.Schema.ObjectId , ref: "user_auth", required: true },
     thumbnail: {type: String, required: true},
     title: {type: String, required: true, unique: true, trim: true},
     subtitle: {type: String, required: true, trim: true},
     description: {type: String, required: true, trim: true},
     category: {type: String, required: true, trim: true},
     isPublish: {type: Boolean, default: false},
+    comments: [
+      {
+        name: {type: String, required: true, trim: true},
+        comment: {type: String, required: true, trim: true},
+        createdAt: {type: Date, default: Date.now},
+      },
+    ],
   },
   {timestamps: true},
 )
