66a215cfb4d578898f86cf9a3cf1ba195100bb88
Hammad Gul <iamhammad148@gmail.com>
2026-04-29 19:36:48 +0500

    add token-based protected route in AdminLayout

--- Frontend/src/Admin/Components/AdminLayout.jsx
+++ Frontend/src/Admin/Components/AdminLayout.jsx
@@ -1,20 +1,28 @@
 import style from "../Components Style/AdminLayout.module.css"
 import Sidebar from "./Sidebar"
 import Navbar from "../../Components/Navbar"
-import { Outlet } from "react-router-dom"
+import {Outlet, useNavigate} from "react-router-dom"
+import {useEffect} from "react"
 
 function AdminLayout() {
-    return (
-        <div className={style["admin-layout"]}>
-            <Navbar />
-            <div className={style["admin-body"]}>
-                <Sidebar />
-                <div className={style["admin-content"]}>
-                    <Outlet />
-                </div>
-            </div>
+  let navigate = useNavigate()
+  let token = localStorage.getItem("token")
+  useEffect(() => {
+    if (!token) navigate("/auth")
+  }, [])
+  if (!token) return null
+
+  return (
+    <div className={style["admin-layout"]}>
+      <Navbar />
+      <div className={style["admin-body"]}>
+        <Sidebar />
+        <div className={style["admin-content"]}>
+          <Outlet />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default AdminLayout
