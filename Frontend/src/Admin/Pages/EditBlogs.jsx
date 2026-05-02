989121a82299a797c1c3de98547335d62678f1c7
Hammad Gul <iamhammad148@gmail.com>
2026-05-02 16:55:12 +0500

    connect EditBlogs with blog API

--- Frontend/src/Admin/Pages/EditBlogs.jsx
+++ Frontend/src/Admin/Pages/EditBlogs.jsx
@@ -1,38 +1,47 @@
 import style from "../Pages Style/EditBlogs.module.css"
-import { useNavigate } from "react-router-dom"
-
-const tempBlogs = [
-    { id: 1, title: "The Rise of Artificial Intelligence in Modern Technology" },
-    { id: 2, title: "Importance of Tourism in Today's World" },
-    { id: 3, title: "The New Way of Study" },
-    { id: 4, title: "Taxes on Luxury Houses" },
-    { id: 5, title: "Maximizing Returns in Startup" },
-    { id: 6, title: "Does AI Replace Developers" },
-]
+import {useNavigate} from "react-router-dom"
+import {useQuery} from "@tanstack/react-query"
+import apiFetch from "../../helper/apiFetch"
 
 function EditBlogs() {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    return (
-        <div className={style["editblogs"]}>
-            <div className={style["editblogs-wrapper"]}>
-                <h2>Edit Blogs</h2>
-                <div className={style["blog-list"]}>
-                    {tempBlogs.map((blog) => (
-                        <div key={blog.id} className={style["blog-row"]}>
-                            <p><strong>Blog :</strong> {blog.title}</p>
-                            <button
-                                className={style["edit-btn"]}
-                                onClick={() => navigate(`/admin/edit-blog/${blog.id}`)}
-                            >
-                                Edit
-                            </button>
-                        </div>
-                    ))}
-                </div>
-            </div>
+  let adminBlogs = useQuery({
+    queryKey: ["admin-blog"],
+    queryFn: async () => {
+      let apiData = await apiFetch(`${import.meta.env.VITE_BACKEND_URL}/blog`)
+      return apiData
+    },
+    refetchOnWindowFocus: false,
+  })
+
+  return (
+    <div className={style["editblogs"]}>
+      <div className={style["editblogs-wrapper"]}>
+        <h2>Edit Blogs</h2>
+        <div className={style["blog-list"]}>
+          {Array.isArray(adminBlogs.data?.adminBlog) &&
+          adminBlogs.data.adminBlog.length > 0 ? (
+            adminBlogs.data.adminBlog.map((blog) => (
+              <div key={blog._id} className={style["blog-row"]}>
+                <p>
+                  <strong>Blog :</strong> {blog.title}
+                </p>
+                <button
+                  className={style["edit-btn"]}
+                  onClick={() => navigate(`/admin/edit-blog/${blog._id}`)}
+                >
+                  Edit
+                </button>
+              </div>
+            ))
+          ) : (
+            <p className={style["no-blogs"]}>No Blogs Found</p>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default EditBlogs
