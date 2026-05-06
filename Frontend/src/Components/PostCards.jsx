445b51aa843ec3f7ad714b64dc8fc0473453eed9
Hammad Gul <iamhammad148@gmail.com>
2026-05-06 19:55:24 +0500

    update PostCards UI with dynamic data

--- Frontend/src/Components/PostCards.jsx
+++ Frontend/src/Components/PostCards.jsx
@@ -1,23 +1,18 @@
 import style from "../Components Style/PostCards.module.css"
 import img from "../assets/1.jpg"
 
-function PostCards() {
-    return (
-        <div className={style["post-card"]}>
-            <div className={style["post-img"]}>
-                <img src={img} alt="bg img" />
-            </div>
-            <div className={style["post-info"]}>
-                <span className={style["post-category"]}>Education</span>
-                <h3 className={style["post-title"]}>
-                    this is my post dont touch this
-                </h3>
-                <p className={style["post-subtitle"]}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nam commodi
-                </p>
-            </div>
-        </div>
-    )
+function PostCards({blog}) {
+  return (
+    <div className={style["post-card"]}>
+      <div className={style["post-img"]}>
+        <img src={blog.thumbnail} alt="blog thumbnail" />
+      </div>
+      <div className={style["post-info"]}>
+        <h3 className={style["post-title"]}>{blog.title}</h3>
+        <p className={style["post-subtitle"]}>{blog.subtitle}</p>
+      </div>
+    </div>
+  )
 }
 
 export default PostCards
