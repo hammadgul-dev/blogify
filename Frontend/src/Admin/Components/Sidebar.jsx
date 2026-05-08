bd707c5a6eba3d9c99027ee1f8d453baf50f0d30
Hammad Gul <iamhammad148@gmail.com>
2026-05-08 17:01:16 +0500

    add logout to admin sidebar

--- Frontend/src/Admin/Components/Sidebar.jsx
+++ Frontend/src/Admin/Components/Sidebar.jsx
@@ -1,49 +1,86 @@
 import style from "../Components Style/Sidebar.module.css"
-import { MdDashboard } from "react-icons/md";
-import { MdAddBox } from "react-icons/md";
-import { MdEditDocument } from "react-icons/md";
-import { FaComments } from "react-icons/fa";
-import { MdManageAccounts } from "react-icons/md";
-import { MdDelete } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
-import { TbLayoutDashboardFilled } from "react-icons/tb";
-import { MdOutlineLibraryAdd } from "react-icons/md";
-import { TbEdit } from "react-icons/tb";
-import { FaRegCommentDots } from "react-icons/fa";
-import { AiOutlineDelete } from "react-icons/ai";
-import { RxDashboard } from "react-icons/rx";
+import {MdDashboard} from "react-icons/md"
+import {MdAddBox} from "react-icons/md"
+import {MdEditDocument} from "react-icons/md"
+import {FaComments} from "react-icons/fa"
+import {MdManageAccounts} from "react-icons/md"
+import {MdDelete} from "react-icons/md"
+import {useNavigate} from "react-router-dom"
+import {TbLayoutDashboardFilled} from "react-icons/tb"
+import {MdOutlineLibraryAdd} from "react-icons/md"
+import {TbEdit} from "react-icons/tb"
+import {FaRegCommentDots} from "react-icons/fa"
+import {AiOutlineDelete} from "react-icons/ai"
+import {RxDashboard} from "react-icons/rx"
+import {AiOutlineLogout} from "react-icons/ai"
+import {useQueryClient} from "@tanstack/react-query"
+import {useDispatch} from "react-redux"
+import {setMessage} from "../../Redux/Slice/NotificationSlice.js"
 
 function Sidebar() {
-    let navigate = useNavigate()
+  let navigate = useNavigate()
+  let queryClient = useQueryClient()
+  let dispatch = useDispatch()
 
-    return (
-        <div className={style["sidebar"]}>
-            <div className={style["sidebar-top"]}>
-                <div className={style["sidebar-item"]} onClick={() => navigate("dashboard")}>
-                    <RxDashboard />
-                    <span>Dashboard</span>
-                </div>
-                <div className={style["sidebar-item"]} onClick={() => navigate("add-blog")}>
-                    <MdOutlineLibraryAdd />
-                    <span>Add Blogs</span>
-                </div>
-                <div className={style["sidebar-item"]} onClick={() => navigate("edit-blog")}>
-                    <TbEdit />
-                    <span>Edit Blogs</span>
-                </div>
-            </div>
-            <div className={style["sidebar-bottom"]}>
-                <div className={style["sidebar-item"]} onClick={() => navigate("comments")}>
-                    <FaRegCommentDots />
-                    <span>Comments</span>
-                </div>
-                <div className={style["sidebar-item"]} onClick={() => navigate("trash")}>
-                    <AiOutlineDelete />
-                    <span>Trash Bin</span>
-                </div>
-            </div>
+  function handleLogout() {
+    localStorage.removeItem("token")
+    queryClient.clear()
+    dispatch(setMessage("Logout Successful"))
+    setTimeout(() => {
+      navigate("/")
+    }, 2600)
+  }
+
+  return (
+    <div className={style["sidebar"]}>
+      <div className={style["sidebar-top"]}>
+        <div
+          className={style["sidebar-item"]}
+          onClick={() => navigate("dashboard")}
+        >
+          <RxDashboard />
+          <span>Dashboard</span>
         </div>
-    )
+        <div
+          className={style["sidebar-item"]}
+          onClick={() => navigate("add-blog")}
+        >
+          <MdOutlineLibraryAdd />
+          <span>Add Blogs</span>
+        </div>
+        <div
+          className={style["sidebar-item"]}
+          onClick={() => navigate("edit-blog")}
+        >
+          <TbEdit />
+          <span>Edit Blogs</span>
+        </div>
+      </div>
+      <div className={style["sidebar-bottom"]}>
+        <div
+          className={style["sidebar-item"]}
+          onClick={() => navigate("comments")}
+        >
+          <FaRegCommentDots />
+          <span>Comments</span>
+        </div>
+        <div
+          className={style["sidebar-item"]}
+          onClick={() => navigate("trash")}
+        >
+          <AiOutlineDelete />
+          <span>Trash Bin</span>
+        </div>
+        <div
+          className={`${style["sidebar-item"]} ${style["logout"]}`}
+          onClick={handleLogout}
+        >
+          <AiOutlineLogout />
+          <span>Logout</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
