1464c8260577dc761f271ba922bd25a7ff8bf487
Hammad Gul <iamhammad148@gmail.com>
2026-05-08 17:54:22 +0500

    add GoogleSuccess redirect handler

---
+++ Frontend/src/Components/GoogleSuccess.jsx
@@ -0,0 +1,28 @@
+import {useEffect} from "react"
+import {useNavigate} from "react-router-dom"
+import {useDispatch} from "react-redux"
+import {setMessage} from "../Redux/Slice/NotificationSlice"
+
+function GoogleSuccess() {
+  let navigate = useNavigate()
+  let dispatch = useDispatch()
+
+  useEffect(() => {
+    let params = new URLSearchParams(window.location.search)
+    let token = params.get("token")
+    let userName = params.get("userName")
+
+    if (token) {
+      localStorage.setItem("token", token)
+      localStorage.setItem("userName", userName)
+      dispatch(setMessage("Login Successful"))
+      navigate("/admin/dashboard", {replace: true})
+    } else {
+      navigate("/auth", {replace: true})
+    }
+  }, [])
+
+  return <p>Redirecting...</p>
+}
+
+export default GoogleSuccess
