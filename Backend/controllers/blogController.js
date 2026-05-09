1a4ceeea08467736b3ee1d6425de37e8097d5dd9
Hammad Gul <iamhammad148@gmail.com>
2026-05-09 18:40:06 +0500

    add category filter to public blogs

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -108,7 +108,7 @@ async function getPublicBlogs(req, resp) {
   try {
     let blog = await blogModel
       .find({isDeleted: false, isPublish: true})
-      .select("thumbnail title subtitle")
+      .select("thumbnail title subtitle category")
     return resp.status(200).json({success: true, blog})
   } catch (e) {
     return resp.status(500).json({message: "Failed to fetch blogs"})
--- Frontend/src/Components/PostLists.jsx
+++ Frontend/src/Components/PostLists.jsx
@@ -1,8 +1,11 @@
-import {useMutation, useQuery} from "@tanstack/react-query"
+import {useQuery} from "@tanstack/react-query"
+import {useSelector} from "react-redux"
 import style from "../Components Style/PostLists.module.css"
 import PostCards from "./PostCards"
 
 function PostLists() {
+  let currCategory = useSelector((state) => state.postFilter.category)
+
   let {data} = useQuery({
     queryKey: ["public-post"],
     queryFn: async () => {
@@ -11,12 +14,16 @@ function PostLists() {
     },
     refetchOnWindowFocus: false,
   })
-  console.log(data?.blog)
+  console.log(data)
+
+  let filteredBlogs = currCategory
+    ? data?.blog?.filter((b) => b.category === currCategory.toLowerCase())
+    : data?.blog
 
   return (
     <div className={style["blogList-section"]}>
-      {Array.isArray(data?.blog) && data.blog.length > 0 ? (
-        data.blog.map((blog) => <PostCards key={blog._id} blog={blog} />)
+      {Array.isArray(filteredBlogs) && filteredBlogs.length > 0 ? (
+        filteredBlogs.map((blog) => <PostCards key={blog._id} blog={blog} />)
       ) : (
         <p className={style["no-blogs"]}>No posts published yet</p>
       )}
