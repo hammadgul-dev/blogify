d0d7f98b0570320cbb303e506d7c3d5a3e5e9786
Hammad Gul <iamhammad148@gmail.com>
2026-05-10 21:39:13 +0500

    search redux setup with dispatch

--- Frontend/src/Components/HeroSection.jsx
+++ Frontend/src/Components/HeroSection.jsx
@@ -1,7 +1,11 @@
 import style from "../Components Style/HeroSection.module.css"
 import {PiShootingStarDuotone} from "react-icons/pi"
+import {setSearch} from "../Redux/Slice/SearchSlice"
+import {useDispatch} from "react-redux"
 
 function HeroSection() {
+  let dispatch = useDispatch()
+
   return (
     <div className={style["hero-section"]}>
       <div className={style["hero-msg"]}>
@@ -22,7 +26,13 @@ function HeroSection() {
         with readers who care.
       </div>
       <div className={style["search-box"]}>
-        <input type="text" placeholder="Search Blog Posts" />
+        <input
+          type="text"
+          placeholder="Search Blog Posts"
+          onChange={(e) =>
+            dispatch(setSearch(e.target.value.trim().toLowerCase()))
+          }
+        />
         <button>Search</button>
       </div>
     </div>
--- Frontend/src/Redux/Store.jsx
+++ Frontend/src/Redux/Store.jsx
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit"
 import notificationReducer from "./Slice/NotificationSlice"
 import postsFilterReducer from "./Slice/PostsFilters"
+import searchSliceReducer from "./Slice/SearchSlice"
 
 let blogifyStore = configureStore({
   reducer: {
     notification: notificationReducer,
     postFilter: postsFilterReducer,
+    userSearch: searchSliceReducer,
   },
 })
 
