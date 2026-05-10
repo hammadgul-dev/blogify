87edfd4ef7ce8fa6938593065e856dcde9a7f8c2
Hammad Gul <iamhammad148@gmail.com>
2026-05-10 23:32:53 +0500

    apply data-theme via AppWrapper for dark/light mode

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,24 +1,35 @@
-import { createRoot } from "react-dom/client";
-import "./Pages Style/Home.css";
-import { RouterProvider } from "react-router-dom";
-import routes from "./Routes";
-import { Provider } from "react-redux";
-import blogifyStore from "./Redux/Store";
-import Notification from "./Components/Notification";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {createRoot} from "react-dom/client"
+import "./Pages Style/Home.css"
+import {RouterProvider} from "react-router-dom"
+import routes from "./Routes"
+import {Provider} from "react-redux"
+import blogifyStore from "./Redux/Store"
+import Notification from "./Components/Notification"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {useSelector} from "react-redux"
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 3,
     },
   },
-});
+})
+
+function AppWrapper() {
+  let mode = useSelector((state) => state.theme.mode)
+  return (
+    <div data-theme={mode}>
+      <Notification />
+      <RouterProvider router={routes} />
+    </div>
+  )
+}
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={blogifyStore}>
-      <Notification />
-      <RouterProvider router={routes}></RouterProvider>
+      <AppWrapper />
     </Provider>
   </QueryClientProvider>,
-);
+)
