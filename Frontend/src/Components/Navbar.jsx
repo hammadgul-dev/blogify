86d2694d00915559f45abed2b0fdcac211b5688c
Hammad Gul <iamhammad148@gmail.com>
2026-05-10 23:18:47 +0500

    dark/light mode toggle with redux

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,12 +1,18 @@
 import style from "../Components Style/Navbar.module.css"
 import {FaBlog} from "react-icons/fa"
-import {CiLight} from "react-icons/ci"
+import {IoSunnyOutline} from "react-icons/io5"
 import {useNavigate} from "react-router-dom"
 import {useQuery} from "@tanstack/react-query"
 import apiFetch from "../helper/apiFetch.js"
+import {useDispatch, useSelector} from "react-redux"
+import {toggleTheme} from "../Redux/Slice/ThemeSlice"
+import {MdOutlineDarkMode} from "react-icons/md"
 
 function Navbar() {
   let navigate = useNavigate()
+  let dispatch = useDispatch()
+  let mode = useSelector((state) => state.theme.mode)
+  console.log(mode)
 
   let {data} = useQuery({
     queryKey: ["verify-user"],
@@ -35,8 +41,8 @@ function Navbar() {
         </div>
       </div>
       <div className={style["right-section"]}>
-        <span>
-          <CiLight />
+        <span onClick={() => dispatch(toggleTheme())}>
+          {mode === "light" ? <MdOutlineDarkMode /> : <IoSunnyOutline />}
         </span>
         {data?.user ? (
           <button onClick={() => navigate("/admin")}>Admin &rarr;</button>
--- Frontend/src/Redux/Store.jsx
+++ Frontend/src/Redux/Store.jsx
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit"
 import notificationReducer from "./Slice/NotificationSlice"
 import postsFilterReducer from "./Slice/PostsFilters"
 import searchSliceReducer from "./Slice/SearchSlice"
+import themeReducer from "./Slice/themeSlice"
 
 let blogifyStore = configureStore({
   reducer: {
     notification: notificationReducer,
     postFilter: postsFilterReducer,
     userSearch: searchSliceReducer,
+    theme: themeReducer,
   },
 })
 
