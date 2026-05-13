b8eddbc694b40abf9ab4ab35973bbad16b7d875c
Hammad Gul <iamhammad148@gmail.com>
2026-05-13 16:52:37 +0500

    cache mongoose connection for serverless

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose"
 
+let isConnected = false
+
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Database Connected Succefully")
-    }
-    catch (e) {
-        console.log("Database Connection Failed")
-    }
+  if (isConnected) return
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI)
+    isConnected = db.connections[0].readyState === 1
+    console.log("Database Connected Successfully")
+  } catch (e) {
+    console.log("Database Connection Failed", e)
+  }
 }
 
 export default connectDB
